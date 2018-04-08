7e0b4585a2aae76a0b62c5d057f66f6c4abe1346
Li Ge <gelber.libi@gmail.com>
2018-04-08 15:32:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {"shlomo": "shlomosong"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  playlist;
+  return playlist;
 }
